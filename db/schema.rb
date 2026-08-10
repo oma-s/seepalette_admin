6b1bdec6388131b94e31edea6354f853031deb53
Malte Stiebritz <68898490+oma-s@users.noreply.github.com>
2026-08-10 16:38:47 +0200

    Upgrade to Rails 8.1.3.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_09_153041) do
+ActiveRecord::Schema[8.1].define(version: 2025_07_09_153041) do
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.string "author_type"
     t.integer "author_id"
+    t.string "author_type"
+    t.text "body"
     t.datetime "created_at", null: false
+    t.string "namespace"
+    t.integer "resource_id"
+    t.string "resource_type"
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
@@ -28,67 +28,67 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_09_153041) do
   create_table "addresses", force: :cascade do |t|
     t.string "address_line1", null: false
     t.string "address_line2"
-    t.string "zip", null: false
-    t.string "city", null: false
-    t.string "addressable_type"
     t.integer "addressable_id"
+    t.string "addressable_type"
+    t.string "city", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zip", null: false
     t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
+    t.datetime "created_at", null: false
     t.text "description"
     t.integer "product_family_id"
-    t.datetime "created_at", null: false
+    t.string "title", null: false
     t.datetime "updated_at", null: false
     t.index ["product_family_id"], name: "index_categories_on_product_family_id"
   end
 
   create_table "expenses", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.date "date"
-    t.string "start_address", null: false
-    t.string "end_address", null: false
-    t.text "purpose", null: false
-    t.integer "km", null: false
-    t.decimal "factor", precision: 3, scale: 2, null: false
     t.datetime "created_at", null: false
+    t.date "date"
+    t.string "end_address", null: false
+    t.decimal "factor", precision: 3, scale: 2, null: false
+    t.integer "km", null: false
+    t.text "purpose", null: false
+    t.string "start_address", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "product_families", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "description"
     t.datetime "created_at", null: false
+    t.text "description"
+    t.string "title", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "menu_description"
-    t.decimal "ekp"
-    t.decimal "uvp"
-    t.decimal "vkp"
-    t.string "stock_unit"
-    t.integer "stock_target"
-    t.boolean "print_on_menu"
     t.boolean "active", default: false
     t.integer "category_id"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.decimal "ekp"
+    t.text "menu_description"
+    t.boolean "print_on_menu"
+    t.integer "stock_target"
+    t.string "stock_unit"
     t.integer "supplier_id"
+    t.string "title", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "uvp"
+    t.decimal "vkp"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["supplier_id"], name: "index_products_on_supplier_id"
   end
 
   create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.integer "resource_id"
     t.datetime "created_at", null: false
+    t.string "name"
+    t.integer "resource_id"
+    t.string "resource_type"
     t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
@@ -96,29 +96,29 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_09_153041) do
   end
 
   create_table "suppliers", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "description"
-    t.string "website"
     t.string "contact_email"
     t.string "contact_phone"
+    t.datetime "created_at", null: false
+    t.string "description"
     t.string "personal_contact_name"
     t.string "preffered_time_to_order"
-    t.datetime "created_at", null: false
+    t.string "title", null: false
     t.datetime "updated_at", null: false
+    t.string "website"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "given_name", null: false
-    t.string "family_name"
+    t.datetime "created_at", null: false
     t.string "email", null: false
     t.string "encrypted_password", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
+    t.string "family_name"
+    t.string "given_name", null: false
     t.datetime "locked_at"
-    t.datetime "created_at", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.string "unlock_token"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -126,22 +126,22 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_09_153041) do
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
     t.integer "role_id"
+    t.integer "user_id"
     t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
   create_table "working_hours", force: :cascade do |t|
-    t.date "date"
-    t.datetime "start_at"
-    t.datetime "end_at"
     t.integer "break_minutes"
-    t.integer "duration_minutes"
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
+    t.date "date"
+    t.integer "duration_minutes"
+    t.datetime "end_at"
+    t.datetime "start_at"
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_working_hours_on_user_id"
   end
 
