a11ad97724526694e2d0e0a01fcd644a76c017d6
Malte Stiebritz <68898490+oma-s@users.noreply.github.com>
2026-08-10 17:36:48 +0200

    Add development tooling

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :integer          not null, primary key
+#  address_line1    :string           not null
+#  address_line2    :string
+#  addressable_type :string
+#  city             :string           not null
+#  zip              :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  addressable_id   :integer
+#
+# Indexes
+#
+#  index_addresses_on_addressable  (addressable_type,addressable_id)
+#
+
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
 
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: suppliers
+#
+#  id                      :integer          not null, primary key
+#  contact_email           :string
+#  contact_phone           :string
+#  description             :string
+#  personal_contact_name   :string
+#  preffered_time_to_order :string
+#  title                   :string           not null
+#  website                 :string
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#
+
 class Supplier < ApplicationRecord
   has_many :addresses, as: :addressable, dependent: :destroy
   has_many :products, dependent: :nullify
